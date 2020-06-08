5e9b487c88907dd860984afca7a49e6041120c37
GuninaDD <dasha.gunina@gmail.com>
2020-06-08 14:43:51 +0300

    Home work Lesson 6 done

---
+++ app/src/main/java/com/geekbrains/poplibs4/di/modules/NetworkModule.java
@@ -0,0 +1,47 @@
+package com.geekbrains.poplibs4.di.modules;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.geekbrains.poplibs4.MainActivity;
+import com.geekbrains.poplibs4.rest.RestAPI;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module(includes = ActivityModule.class)
+public class NetworkModule {
+
+    @Provides
+    String baseUrl() {
+        return "https://api.github.com/";
+    }
+
+    @Provides
+    @Singleton
+    Retrofit getRetrofit(String baseUrl) {
+        return new Retrofit.Builder().baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    RestAPI getRestApi(Retrofit retrofit) {
+        return retrofit.create(RestAPI.class);
+    }
+
+    @Provides
+    boolean isConnected(MainActivity activity) {
+        ConnectivityManager manager = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager != null ? manager.getActiveNetworkInfo() : null;
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+}
